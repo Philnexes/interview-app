ea8301b8cbfc2c2029c8da1d1d1de8f389f9454e
Filip Pindej <filip@pindej.cz>
2020-11-16 23:20:30 +0100

    Handle null or undefined posterpath and titlename better

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
 import { getImgBaseUrl } from "../utils/utils";
 
 const Movie = ({ movie, setShowDetail }) => {
+  let moviePosterPath = movie?.poster_path
+    ? `${getImgBaseUrl()}${movie.poster_path}`
+    : "https://www.indiaspora.org/wp-content/uploads/2018/10/image-not-available.jpg";
+  let movieTitle = movie?.title || movie?.name || movie?.originalName;
+
   return (
     <img
       onClick={() => setShowDetail(true)}
       className="movie-poster"
-      src={
-        movie.poster_path !== null
-          ? `${getImgBaseUrl()}${movie.poster_path}`
-          : ""
-      }
-      alt={movie?.title || movie?.name || movie?.originalName}
+      src={moviePosterPath}
+      alt={movieTitle}
     />
   );
 };
