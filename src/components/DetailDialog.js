4a0cd36e3d450087bf1f4b2a279fe40da66665b7
Filip Pindej <filip@pindej.cz>
2020-11-25 02:00:16 +0100

    Include mux.js dependency for shaka video player to be able to stream m3u8 in Chrome

--- src/components/DetailDialog.js
+++ src/components/DetailDialog.js
@@ -1,25 +1,37 @@
+import { useState } from "react";
 import { getImgBaseUrl } from "../utils/utils";
+import VideoPlayer from "./VideoPlayer";
 
 const DetailDialog = ({ movieDetail }) => {
+  const [playTrailer, setPlayTrailer] = useState(true);
+
   let moviePosterPath = movieDetail?.poster_path
     ? `${getImgBaseUrl()}${movieDetail.poster_path}`
     : "https://www.indiaspora.org/wp-content/uploads/2018/10/image-not-available.jpg";
   let movieTitle =
     movieDetail?.title || movieDetail?.name || movieDetail?.originalName;
 
+  const handlePlay = () => {
+    setPlayTrailer(true);
+  };
+
   return (
     <div className="modal-content">
-      <div className="movie-overview">
+      <div className="video-player">{playTrailer && <VideoPlayer />}</div>
+
+      {/* <div className="movie-overview">
         <h2 className="title">{movieTitle}</h2>
         <p className="overview">{movieDetail?.overview}</p>
         <div className="play-button-container">
-          <button className="play-button">Play trailer</button>
+          <button onClick={handlePlay} className="play-button">
+            Play trailer
+          </button>
         </div>
       </div>
 
       <div className="thumbnail-container">
         <img className="thumbnail" src={moviePosterPath} alt="Obrazek" />
-      </div>
+      </div> */}
     </div>
   );
 };
