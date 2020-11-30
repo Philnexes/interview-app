b5c4fb26a7b7e2f6ca20ebcd2e846e0d6cb9f531
Filip Pindej <filip@pindej.cz>
2020-11-30 20:47:06 +0100

    Improve styling and responsiveness a bit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -17,17 +17,30 @@ const Nav = () => {
     <div
       id="navbar"
       className={`nav ${showNav && "nav-background-scrolldown"}`}
-    >
-      <img
-        className="nav-logo"
-        src="https://www.24i.com/wp-content/themes/24i_media/images/24i%20Logo%20%E2%80%93%20White.svg"
-        alt="24i logo"
-      />
-      <img
-        className="nav-avatar"
-        src="http://assets.pindej.cz/photo.jpg"
-        alt="avatar"
-      />
+          <a
+        className="nav-logo-container"
+        href="https://www.24i.com/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          className="nav-logo"
+          src="https://www.24i.com/wp-content/themes/24i_media/images/24i%20Logo%20%E2%80%93%20White.svg"
+          alt="24i logo"
+        />
+      </a>
+      <a
+        className="nav-avatar-container"
+        href="https://www.linkedin.com/in/filip-dorian-pindej-451b34bb/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img
+          className="nav-avatar"
+          src="http://assets.pindej.cz/photo.jpg"
+          alt="avatar"
+        />
+      </a>
     </div>
   );
 };
